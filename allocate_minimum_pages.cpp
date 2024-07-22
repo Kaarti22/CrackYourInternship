c5a30eadb4ba2c41e9af3bed45205eebff161363
Kaarti22 <kaartimondi@gmail.com>
2024-07-22 23:20:35 +0530

    small syntax error

--- allocate_minimum_pages.cpp
+++ allocate_minimum_pages.cpp
@@ -26,15 +26,8 @@ Constraints:
 1 <= arr[i] <= 106 
 */
 
-//{ Driver Code Starts
-// Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
-
-
-// } Driver Code Ends
-// User function template in C++
 
 class Solution {
   private:
@@ -75,25 +68,3 @@ class Solution {
         return low;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int A[n];
-        for (int i = 0; i < n; i++) {
-            cin >> A[i];
-        }
-        int m;
-        cin >> m;
-        Solution ob;
-        cout << ob.findPages(n, A, m) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
