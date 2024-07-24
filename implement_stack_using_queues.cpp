709fd313fe14647e4688d9dfea96e9b4c6c64bef
Kaarti22 <kaartimondi@gmail.com>
2024-07-24 22:50:21 +0530

    Implement Stack using Queues

--- implement_stack_using_queues.cpp
+++ implement_stack_using_queues.cpp
@@ -42,3 +42,48 @@ All the calls to pop and top are valid.
 Follow-up: Can you implement the stack using only one queue?
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int>q;
+
+    MyStack() {
+        while(!q.empty()){
+            q.pop();
+        }
+    }
+    
+    void push(int x) {
+        int n = q.size();
+        q.push(x);
+        while(n--){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
