ac3e8c9ee5ce272aa80728728b79793e3547c535
Kaarti22 <kaartimondi@gmail.com>
2024-08-13 22:32:20 +0530

    maximum product subarray

---
+++ maximum_product_subarray.cpp
@@ -0,0 +1,52 @@
+/*
+Link: https://leetcode.com/problems/maximum-product-subarray/
+
+Given an integer array nums, find a 
+subarray
+ that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = (long long)*max_element(nums.begin(), nums.end());
+        long long currMin = 1, currMax = 1;
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0){
+                currMin = 1, currMax = 1;
+                continue;
+            }
+            long long temp = currMax * nums[i] * 1ll;
+            currMax = max({currMax * nums[i], currMin * nums[i], nums[i] * 1ll});
+            currMin = min({temp, currMin * nums[i], nums[i] * 1ll});
+            ans = max(ans, currMax);
+        }
+        return (int)ans;
+    }
+};
